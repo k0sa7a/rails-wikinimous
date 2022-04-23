15aac6730a0581bc932379bf4e25cc4b42473649
Stefan Filev <stefan.s.filev@gmail.com>
2022-04-23 15:44:50 +0100

    Added CRUD actions and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  rand_content = ''
+  rand(2..4).times do
+    rand_content += Faker::Lorem.paragraph(sentence_count: 15, supplemental: false, random_sentences_to_add: 7)
+  end
+  Article.create(
+    title: "#{Faker::Beer.brand} #{Faker::Beer.style} #{Faker::Beer.alcohol}",
+    content: rand_content
+  )
+end
